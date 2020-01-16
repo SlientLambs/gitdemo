cc64b72e370208f9ebbad1dcb138eb853bc57209
Ji Song <1073280183@qq.com>
2020-01-16 10:11:44 +0800

    Refactoring function to a test case.

--- demo.py
+++ demo.py
@@ -1,4 +1,10 @@
-def hello():
-    print('Hello,world!')
+import unittest
 
-hello()
+
+class TestHello(unittest.TestCase):
+    def test_hello(self):
+        self.assertEqual(1, 1)
+
+
+if __name__ == '__main__':
+    unittest.main()
