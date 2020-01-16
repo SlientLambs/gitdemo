2169969dde958be923173afd2db835bf6e68491a
Ji Song <1073280183@qq.com>
2020-01-16 13:44:28 +0800

    Refactoring to an automation test case.

--- demo.py
+++ demo.py
@@ -1,9 +1,29 @@
+from selenium.webdriver import Chrome
+from time import sleep
 import unittest
 
 
 class TestHello(unittest.TestCase):
+    def setUp(self):
+        self.driver = Chrome()
+        self.base_url = 'http://192.168.0.238/'
+        self.driver.maximize_window()
+        self.driver.set_script_timeout(10)
+        self.driver.implicitly_wait(10)
+
     def test_hello(self):
-        self.assertEqual(1, 1)
+        driver = self.driver
+        driver.get(self.base_url)
+        driver.find_element_by_xpath('//input[@name="username"]').send_keys('ddzx')
+        driver.find_element_by_xpath('//input[@name="password"]').send_keys('123')
+        driver.find_element_by_tag_name('button').click()
+        sleep(1)
+        except_name = '调度中心负责人'
+        actual_name = driver.find_element_by_xpath('//div[@class="info"]').text
+        self.assertEqual(except_name, actual_name)
+
+    def tearDown(self):
+        self.driver.quit()
 
 
 if __name__ == '__main__':
