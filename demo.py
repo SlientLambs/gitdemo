5a396cec1bccc1985307ca80b84c2cde8984aabf
Ji Song <1073280183@qq.com>
2020-01-19 11:25:10 +0800

    Change class name method name and test data.

--- demo.py
+++ demo.py
@@ -3,7 +3,7 @@ from time import sleep
 import unittest
 
 
-class TestHello(unittest.TestCase):
+class TestLogin(unittest.TestCase):
     def setUp(self):
         self.driver = Chrome()
         self.base_url = 'http://192.168.0.238/'
@@ -11,14 +11,14 @@ class TestHello(unittest.TestCase):
         self.driver.set_script_timeout(10)
         self.driver.implicitly_wait(10)
 
-    def test_hello(self):
+    def test_login(self):
         driver = self.driver
         driver.get(self.base_url)
-        driver.find_element_by_xpath('//input[@name="username"]').send_keys('ddzx')
+        driver.find_element_by_xpath('//input[@name="username"]').send_keys('ddzx2')
         driver.find_element_by_xpath('//input[@name="password"]').send_keys('123')
         driver.find_element_by_tag_name('button').click()
         sleep(1)
-        except_name = '调度中心负责人'
+        except_name = '调度中心'
         actual_name = driver.find_element_by_xpath('//div[@class="info"]').text
         self.assertEqual(except_name, actual_name)
 
